380e106fd5166bdc3877125d0a209ba679b868df
varsha02022006-max <varsha02022006@gmail.com>
2025-09-24 11:42:48 +0530

    Add files via upload

---
+++ EX6.1.py
@@ -0,0 +1,25 @@
+
+OPERATORS=set(['+','-','*','/','(',')','^'])
+PRIORITY={'+':1,'-':1,'*':2,'/':2,'^':3}
+def infix_to_postfix(expression):
+    stack=[]
+    output=''
+    for ch in expression:
+        if ch not in OPERATORS:
+            output+=ch
+        elif ch=='(':
+            stack.append('(')
+        elif ch==')':
+            while stack and stack[-1]!='(':
+                output+=stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1]!='(' and PRIORITRY<=PRIORITY[stack[-1]]:
+                output+=stack.pop()
+            stack.append(ch)
+        while stack:
+            output+=stack.pop()
+        return output
+expression=input('Enter infix expression')
+print('infix expression:',expression)
+print('postfix expression:',infix_to_postfix(expression))
